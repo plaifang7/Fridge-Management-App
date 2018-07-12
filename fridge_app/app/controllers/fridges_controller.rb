bcf12c8f3c8d94f987df12659e2f0c705d7bf78d
plaifang7 <plaifang7@gmail.com>
2018-07-11 20:49:20 -0400

    FULL CRUD on fridges in routes. moving on to views to render information

--- fridge_app/app/controllers/fridges_controller.rb
+++ fridge_app/app/controllers/fridges_controller.rb
@@ -1,3 +1,55 @@
 class FridgesController < ApplicationController
-  
+  def index
+  @fridges = Fridge.all
+  end
+
+  def show
+    @fridges = Fridge.find(params[:id])
+  end
+
+  def new
+    @fridge = Fridge.new
+  end
+
+  def create
+    @fridge = Fridge.create(fridge_params)
+    if @fridge.valid?
+      @fridge.save
+      redirect_to fridge_path(@fridge)
+    else
+      flash[:notice] = "Please fill out all fields"
+      redirect_to '/fridges/new'
+    end
+  end
+
+  def edit
+    @fridge = Fridge.find(params[:id])
+  end
+
+  def update
+    @fridge = Fridge.find(params[:id])
+    @fridge.update_attributes(fridge_params)
+    if @fridge.valid?
+      @fridge.save
+      redirect_to fridge_path(@fridge)
+    else
+      flash[:notice] = "Pleas update all fields"
+      redirect_to edit_fridge_path(@fridge)
+    end
+  end
+
+  def destroy
+    @firdge = Fridge.find(params[:id]).destroy
+    redirect_to fridges_path
+  end
+
+
+
+  private
+
+  def fridge_params
+    params.require(:fridge).permit(:brand, :location, :size)
+  end
+
+
 end
